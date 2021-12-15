9f09ba52819fa713509c1fc8f40f51726947c152
Pooja SAXENA <nrjrasaxena@gmail.com>
2021-12-15 09:26:54 +0100

    modified 1. ylim on left plot 2. line style

--- pool_common_scripts/ploting_script.py
+++ pool_common_scripts/ploting_script.py
@@ -16,14 +16,14 @@ def plot_n_model(num_history=2, name_history=[],
           is_sparse_categorical=False,       
           is_saved_history=False):
     """
-    num_history  : no of history to be plotted
-    name_history : list of history name, ex [history_1, history_2]
-    label_list   : list of label names, ex ['simple_model', 'model_cnn']
-    y_lim_low    : lowest y scale
-    y_lim_high   : highest y scale
-    leg_loc      : location of legend ex: 'lower left', 'lower right', 'upper right', 'upper left'
+    num_history           : no of history to be plotted
+    name_history          : list of history name, ex [history_1, history_2]
+    label_list            : list of label names, ex ['simple_model', 'model_cnn']
+    y_lim_low             : lowest y scale
+    y_lim_high            : highest y scale
+    leg_loc               : location of legend ex: 'lower left', 'lower right', 'upper right', 'upper left'
     is_sparse_categorical : True, if sparse_categorical_accuracy is being used. 
-    is_saved_history : if plotting saved history via pickle
+    is_saved_history      : if plotting saved history via pickle
  
                      @@ Saving part--
                      try:
@@ -44,7 +44,7 @@ def plot_n_model(num_history=2, name_history=[],
     """
     fig = plt.figure(figsize=(15,5))
 
-    assert(len(name_history) == num_history), 'Error, name_history should be of length num_history!'
+    assert(len(name_history) == num_history), 'Error, name_history should be of same length of num_history!'
     
     if is_sparse_categorical:
         acc_variable = ['sparse_categorical_accuracy', 'val_sparse_categorical_accuracy']
@@ -52,42 +52,44 @@ def plot_n_model(num_history=2, name_history=[],
         acc_variable=['accuracy', 'val_accuracy']
     loss_variable = ['loss', 'val_loss']
 
-    color=['red','blue', 'orange', 'green', 'black', 'aqua','yellow']
+    color      = ['red','blue', 'orange', 'green', 'black', 'aqua','yellow']
+    line_style = ["solid", "dashed"]
     
     plt.subplot(1,2,1)
     for index, history in enumerate(name_history):
-        #print(index, history)
 
         if is_saved_history:
-            plt.plot(history[acc_variable[0]], label=label_list[index], c=color[index], ls='-')
-            plt.plot(history[acc_variable[1]], c=color[index],ls='--')
+            plt.plot(history[acc_variable[0]], label=label_list[index], c=color[index], ls=line_style[0])
+            plt.plot(history[acc_variable[1]], c=color[index],ls=line_style[1])
         else:
-            plt.plot(history.history[acc_variable[0]], label=label_list[index], c=color[index], ls='-')
-            plt.plot(history.history[acc_variable[1]], c=color[index],ls='--')            
+            plt.plot(history.history[acc_variable[0]], label=label_list[index], c=color[index], ls=line_style[0])
+            plt.plot(history.history[acc_variable[1]], c=color[index],ls=line_style[1])            
 
         plt.ylabel(acc_variable[0])
         plt.xlabel('Epoch')
         plt.grid(True)
         plt.legend(loc=leg_loc)
-        plt.title('Training - , Validation ---')
+        plt.title('Training - , Validation - -')
         plt.ylim(ylim_low,ylim_high)
 
     plt.subplot(1,2,2)
     for index, history in enumerate(name_history):
 
         if is_saved_history:
-            plt.plot(history[loss_variable[0]], label=label_list[index], c=color[index], ls='-')
-            plt.plot(history[loss_variable[1]],  c=color[index],ls='--')
+            plt.plot(history[loss_variable[0]], label=label_list[index], c=color[index], ls=line_style[0])
+            plt.plot(history[loss_variable[1]],  c=color[index],ls=line_style[1])
         else:
-            plt.plot(history.history[loss_variable[0]], label=label_list[index], c=color[index], ls='-')
-            plt.plot(history.history[loss_variable[1]],  c=color[index],ls='--')
+            plt.plot(history.history[loss_variable[0]], label=label_list[index], c=color[index], ls=line_style[0])
+            plt.plot(history.history[loss_variable[1]],  c=color[index],ls=line_style[1])
             
-        plt.title('Training - , Validation ---')
-
+        plt.title('Training - , Validation - -')
+        plt.ylim(ylim_low,ylim_high)
+        
     plt.xlabel('Epoch')
     plt.ylabel(loss_variable[0])
     plt.grid(True)
     plt.legend(loc=leg_loc)
     plt.ylim(ylim_low,ylim_high)
-
-help(plot_n_model)
+    plt.show()
+    
+##help(plot_n_model)
