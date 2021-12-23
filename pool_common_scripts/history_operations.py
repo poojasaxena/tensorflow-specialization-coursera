aa2606cb5fd73dce526fbec8c0284c82b613e9c3
Pooja SAXENA <nrjrasaxena@gmail.com>
2021-12-23 15:36:21 +0100

    added get_path def

--- pool_common_scripts/history_operations.py
+++ pool_common_scripts/history_operations.py
@@ -5,8 +5,7 @@ import pickle
 saved_model_path = os.environ.get("DirForSavedModel")
 
 def saveHist(project_name, name, history):
-    path = os.path.join(saved_model_path, project_name, name)
-    
+    path = get_complete_path(project_name, name)    
     if os.path.exists(path):
         print(f"{os.path.exists(path)} already exist!")
         return
@@ -15,7 +14,11 @@ def saveHist(project_name, name, history):
             pickle.dump(history.history, file_pi)
 
 
-def loadHist(path):
+def loadHist(project_name, name):
+    path = get_complete_path(project_name, name)
     if os.path.exist(path):
         saved_history= pickle.load(open(path, "rb"))
     return saved_history
+
+def get_complete_path(project_name, name):
+    return os.path.join(saved_model_path, project_name, name)
