6d64fee6f39787e9ada16628460765a49e1be585
Pooja Saxena <36233154+poojasaxena@users.noreply.github.com>
2022-02-16 13:43:54 +0100

    Delete pool_common_scripts directory

--- pool_common_scripts/mkl_testing.py
+++
@@ -1,7 +0,0 @@
-import tensorflow as tf
-major_version = int(tf.__version__.split(".")[0])
-if major_version >= 2:
-    from tensorflow.python import _pywrap_util_port
-    print("MKL enabled:", _pywrap_util_port.IsMklEnabled())
-else:
-    print("MKL enabled:", tf.pywrap_tensorflow.IsMklEnabled()) 
